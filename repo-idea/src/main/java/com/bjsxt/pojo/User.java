2de6e5c9a75a331bf8c0087c12b2145282ace75e
zhanghengwei <291147401@qq.com>
2024-08-05 16:10:35 +0800

    first commit

---
+++ repo-idea/src/main/java/com/bjsxt/pojo/User.java
@@ -0,0 +1,20 @@
+package com.bjsxt.pojo;
+
+/**
+ * @Author zhangHengWei
+ * @Date 2024/8/5 16:07
+ * @PackageName:com.bjsxt.pojo
+ * @ClassName: User
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class User {
+
+    String username;
+
+    String password;
+    String email;
+    String phoneNum;
+
+
+}
